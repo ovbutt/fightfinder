a3b56001893c1ed14646682c5e996a8f968219c7
Ovais Butt <ovaisbutt786@gmail.com>
2021-05-31 16:14:07 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Provider } from 'react-redux'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/firestore' // make sure you add this for firestore
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase'
+import { createFirestoreInstance } from 'redux-firestore'
+import Home from './Home'
+import configureStore from './store'
+import { firebase as fbConfig, rrfConfig } from './config'
+import './App.css'
 
-function App() {
+const initialState = window && window.__INITIAL_STATE__ // set initial state here
+const store = configureStore(initialState)
+// Initialize Firebase instance
+firebase.initializeApp(fbConfig)
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider
+        firebase={firebase}
+        config={rrfConfig}
+        dispatch={store.dispatch}
+        createFirestoreInstance={createFirestoreInstance}>
+        <Home />
+      </ReactReduxFirebaseProvider>
+    </Provider>
+  )
 }
-
-export default App;
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export const firebase = {
+  apiKey: "AIzaSyDFI6AwA2vVZXjBE-7pxQejAlYDASJjMxY",
+  authDomain: "jiujitsu-6ace1.firebaseapp.com",
+  projectId: "jiujitsu-6ace1",
+  storageBucket: "jiujitsu-6ace1.appspot.com",
+  messagingSenderId: "6736206236",
+  appId: "1:6736206236:web:d7a53e8a7acfea43679059",
+  measurementId: "G-5WVGWSDJVD",
+};
+
+export const rrfConfig = {
+  userProfile: "users",
+  useFirestoreForProfile: true, // Store in Firestore instead of Real Time DB
+};
+
+export default { firebase, rrfConfig };
